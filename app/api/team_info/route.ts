1bf11c731d5aa13f44c9f15ebb2f9560e1256374
elhadif3-dot <elhadif3-dot@users.noreply.github.com>
2026-07-22 18:59:52 +0300

    Finalize team info endpoint

--- app/api/team_info/route.ts
+++ app/api/team_info/route.ts
@@ -3,9 +3,9 @@ export function GET() {
     group_batch_order_number: "Batch3_08",
     team_name: "Autonomous Lisbon Airbnb Listing Editor",
     students: [
-      { name: process.env.TEAM_STUDENT_1_NAME ?? "Shoval", email: process.env.TEAM_STUDENT_1_EMAIL ?? "set-email-in-env@example.com" },
-      { name: process.env.TEAM_STUDENT_2_NAME ?? "Daniel", email: process.env.TEAM_STUDENT_2_EMAIL ?? "set-email-in-env@example.com" },
-      { name: process.env.TEAM_STUDENT_3_NAME ?? "Ofel", email: process.env.TEAM_STUDENT_3_EMAIL ?? "set-email-in-env@example.com" }
+      { name: process.env.TEAM_STUDENT_1_NAME ?? "Shoval", email: process.env.TEAM_STUDENT_1_EMAIL ?? "shovalzvieli@campus.technion.ac.il" },
+      { name: process.env.TEAM_STUDENT_2_NAME ?? "Daniel", email: process.env.TEAM_STUDENT_2_EMAIL ?? "edaniel@campus.technion.ac.il" },
+      { name: process.env.TEAM_STUDENT_3_NAME ?? "Opal", email: process.env.TEAM_STUDENT_3_EMAIL ?? "opalzvieli@campus.technion.ac.il" }
     ]
   });
 }
